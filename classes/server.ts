58648fa57c9f8e31d918b3fdcbc52137f1acf58b
marvicgit <martinhuacho@hotmail.com>
2020-09-12 15:30:41 -0500

    seccion 4

--- classes/server.ts
+++ classes/server.ts
@@ -1,16 +1,43 @@
 import express from 'express';
 import { SERVER_PORT } from '../global/environment';
+import socketIO from "socket.io";
+import http from "http";
+import * as socket from '../sockets/socket';
+
 export default class Server {
 
+    static _instance: Server;
     app: express.Application;
     port: number;
+    io: socketIO.Server;
+    httpServer: http.Server;
 
-    constructor() {
+    private constructor() {
         this.app = express();
         this.port = SERVER_PORT
+        this.httpServer = new http.Server(this.app);
+        this.io = socketIO(this.httpServer);
+        this.escucharSockets();
+    }
+
+    static get instance() {
+        return this._instance || (this._instance = new this())
+    }
+
+    private escucharSockets() {
+        console.log('escuchando conexciones - sockets');
+        this.io.on('connection', cliente => {
+            console.log('cliente conectado');
+
+            socket.mensaje(cliente, this.io);
+            //desconectar
+            socket.desconectar(cliente);
+        });
+
+    
     }
 
     start(callback: any) {
-        this.app.listen(this.port, callback);
+        this.httpServer.listen(this.port, callback);
     }
 }
---
+++ sockets/socket.ts
@@ -0,0 +1,17 @@
+import { Socket } from "socket.io";
+import SocketIO from "socket.io";
+
+
+export const desconectar = (cliente: Socket) => {
+    cliente.on('disconnect', () => {
+        console.log('cliente desconectado');
+        
+    });
+}
+
+export const mensaje = (cliente: Socket, io: SocketIO.Server) => {
+    cliente.on('mensaje', (payload: { de: string, cuerpo: string }) => {
+        console.log('mensaje recibido', payload); 
+        io.emit('mensaje-nuevo', payload);
+    });
+}
